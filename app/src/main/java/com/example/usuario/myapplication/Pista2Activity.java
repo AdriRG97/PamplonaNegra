4da62f30a1c78547825c5d028c5c9ef81330c1bf
julencastro37 <julencastro46@gmail.com>
2017-10-25 10:39:26 +0200

    App v2.1.3 Poniendo la lectura de ficheros desde raw ( SOLO LECTURA!) Falta poner bucles para que lea varias lineas etc...

--- app/src/main/java/com/example/usuario/myapplication/Pista2Activity.java
+++ app/src/main/java/com/example/usuario/myapplication/Pista2Activity.java
@@ -4,12 +4,20 @@ import android.app.Activity;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Pista2Activity extends Activity {
     VideoView videoView;
+     TextView pistaTexto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,32 @@ public class Pista2Activity extends Activity {
         MediaController mc = new MediaController(this);
         videoView.setMediaController(mc);
         videoView.start();
+
+        try
+        {
+            InputStream fraw =
+                    getResources().openRawResource(R.raw.pista);
+
+            BufferedReader brin =
+                    new BufferedReader(new InputStreamReader(fraw));
+
+            String linea = brin.readLine();
+            String lineas[]= linea.split("//");
+           pistaTexto = (TextView) findViewById(R.id.txtPista);
+           pistaTexto.setText(lineas[1]);
+
+
+            fraw.close();
+        }
+        catch (Exception ex)
+        {
+            Log.e("Ficheros", "Error al leer fichero desde recurso raw");
+        }
     }
+
+
+
+
 //    public  void PlayVideo(View view){
 //
 //    }
