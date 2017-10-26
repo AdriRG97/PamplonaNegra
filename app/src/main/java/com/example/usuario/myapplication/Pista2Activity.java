873d3a728451ef25591b085377d89e688cb6326a
julencastro37 <julencastro46@gmail.com>
2017-10-26 11:57:42 +0200

    App v2.2 mostrando video cuando es necesario y cuando no ocultandolo, cambiando la pista de Strings a ArrayList para poder tener más de una

--- app/src/main/java/com/example/usuario/myapplication/Pista2Activity.java
+++ app/src/main/java/com/example/usuario/myapplication/Pista2Activity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -13,24 +14,37 @@ import android.widget.VideoView;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Pista2Activity extends Activity {
     VideoView videoView;
      TextView pistaTexto;
+    int pista=0;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pista2);
-         videoView= (VideoView) findViewById(R.id.videoPrueba);
-        Uri directorio = Uri.parse("android.resource://com.example.usuario.myapplication/"+R.raw.intromono);
-        videoView.setVideoURI(directorio);
+        videoView= (VideoView) findViewById(R.id.videoPrueba);
 
-        MediaController mc = new MediaController(this);
-        videoView.setMediaController(mc);
-        videoView.start();
+        //falta otro metodo que lo primero identifique la pista para comprovar que debe haber un VideoView
 
+        if(SaberPista()==0 || SaberPista()==4){
+            MostrarVideo();//aqui falta un condiciona que ponga el video SOLO en las pistas que tienen que tener video
+        }else{
+        OcultarVideo();//rompe y para la aplicación
+        }
+        EscribirPista();
+
+
+
+    }
+
+    private void EscribirPista(){
+        String linea;
+        ArrayList<String> lineas = new ArrayList<String>();
         try
         {
             InputStream fraw =
@@ -38,21 +52,38 @@ public class Pista2Activity extends Activity {
 
             BufferedReader brin =
                     new BufferedReader(new InputStreamReader(fraw));
-
-            String linea = brin.readLine();
-            String lineas[]= linea.split("//");
-           pistaTexto = (TextView) findViewById(R.id.txtPista);
-           pistaTexto.setText(lineas[1]);
-
-
+            //hay que poner un condicional y una lectura de el fichero/BBDD donde tengamos almacenada la puntuación y segun el número de pista en el que esté muestre una u otra
+            do{
+                linea = brin.readLine();
+                lineas.add(linea.split("//")[1]);
+            }while (linea!="");
             fraw.close();
         }
         catch (Exception ex)
         {
             Log.e("Ficheros", "Error al leer fichero desde recurso raw");
         }
-    }
+        pistaTexto = (TextView) findViewById(R.id.txtPista);//quizás falta comprobar que el int que devulve la pista no sobrepasa los limites ya que si eso asi rompreá
+        pistaTexto.setText(lineas.get(pista));
 
+    }
+private void MostrarVideo(){
+
+videoView.setVisibility(View.VISIBLE);
+    Uri directorio = Uri.parse("android.resource://com.example.usuario.myapplication/"+R.raw.intromono);
+    videoView.setVideoURI(directorio);
+
+    MediaController mc = new MediaController(this);
+    videoView.setMediaController(mc);
+    videoView.start();
+}
+private int SaberPista(){
+//hacer este método casi entero para que busque el número de pista y la devuelva
+    return pista;
+}
+private void OcultarVideo(){
+    videoView.setVisibility(View.INVISIBLE);
+}
 
 
 
