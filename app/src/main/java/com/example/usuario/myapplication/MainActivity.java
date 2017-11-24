1214abb198b3bd3d78a53ff5b589b8de8687956f
Adrián Reina Goñi <aereoforce@gmail.com>
2017-11-24 16:47:04 +0100

    V3 Beta Cambio build.gradle version minima api 21 Lollipop Empezando a meter tema Material Design, puede que haya errores Musica en pantalla principal

--- app/src/main/java/com/example/usuario/myapplication/MainActivity.java
+++ app/src/main/java/com/example/usuario/myapplication/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.usuario.myapplication;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,55 +12,72 @@ public class MainActivity extends Activity {
 
 
     Button siguiente, next, conf;
+    MediaPlayer mediaPlayer;
 
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer = MediaPlayer.create(this, R.raw.musicamenu);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mediaPlayer.release();
+    }
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 //                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 //                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 25);
 //                }
+//        mediaPlayer = MediaPlayer.create(this, R.raw.musicamenu);
+//        mediaPlayer.start();
 
 
-            siguiente = (Button)findViewById(R.id.button2);
-                siguiente.setOnClickListener(new View.OnClickListener() {
+        siguiente = (Button) findViewById(R.id.button2);
+        siguiente.setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View v) {
-                Intent siguiente = new Intent(MainActivity.this , InfoActivity.class);
-                        startActivity(siguiente);
-                    }
-                });
-
-                next = (Button)findViewById(R.id.button);
-                next.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-
-
-                        //--hay que comprobar que la ubicación esta activada (no el permiso) si no da 'error' ( no error pero no funciona)
-
-
-                        Intent siguiente = new Intent(MainActivity.this , Jugar.class);
-                        startActivity(siguiente);
-                    }
-                });
-
-                conf = (Button)findViewById(R.id.btnAjustes);
-                conf.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        Intent conf = new Intent(MainActivity.this , Configuraciones.class);
-                        startActivity(conf);
-                    }
-                });
-
-
-
-        }
-
-
-
-
+            @Override
+            public void onClick(View v) {
+                Intent siguiente = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(siguiente);
             }
+        });
+
+        next = (Button) findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                //--hay que comprobar que la ubicación esta activada (no el permiso) si no da 'error' ( no error pero no funciona)
+
+
+                Intent siguiente = new Intent(MainActivity.this, Jugar.class);
+                startActivity(siguiente);
+            }
+        });
+
+        conf = (Button) findViewById(R.id.btnAjustes);
+        conf.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                Intent conf = new Intent(MainActivity.this, Configuraciones.class);
+                startActivity(conf);
+            }
+        });
+
+
+    }
+
+
+}
